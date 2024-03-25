d92e4c5c377892e3717308b567db51230092cd80
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-03-25 14:16:15 -0400

    added data binding + a recycler view

--- app/src/main/java/com/project/organicoutlet/MainActivity.kt
+++ app/src/main/java/com/project/organicoutlet/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.project.organicoutlet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.project.organicoutlet.databinding.ActivityMainBinding
+import com.project.organicoutlet.ui.recyclerview.adapter.ProductsListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.productsList.adapter = ProductsListAdapter()
     }
 }
