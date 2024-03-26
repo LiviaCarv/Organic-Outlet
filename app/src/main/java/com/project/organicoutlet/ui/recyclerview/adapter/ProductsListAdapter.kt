1023ed46b32fe9c805243e12e05c34dc19cb1453
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-03-26 09:11:31 -0400

    added View Binding to RecyclerView

--- app/src/main/java/com/project/organicoutlet/ui/recyclerview/adapter/ProductsListAdapter.kt
+++ app/src/main/java/com/project/organicoutlet/ui/recyclerview/adapter/ProductsListAdapter.kt
@@ -12,14 +12,10 @@ import org.w3c.dom.Text
 
 class ProductsListAdapter(private val products: List<Product>) : RecyclerView.Adapter<ProductsListAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(private val binding: ProductItemBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Product) {
-            val name: TextView = itemView.findViewById(R.id.txt_product_name)
-            name.text = item.name
-            val description: TextView = itemView.findViewById(R.id.txt_description)
-            description.text = item.description
-            val price: TextView = itemView.findViewById(R.id.txt_price)
-            price.text = item.price.toPlainString()
+            binding.product = item
+            binding.executePendingBindings()
         }
 
     }
@@ -27,12 +23,12 @@ class ProductsListAdapter(private val products: List<Product>) : RecyclerView.Ad
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ProductItemBinding.inflate(inflater, parent, false)
-        return ViewHolder(binding.root)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = products[position]
-        holder.bind(item)
+        val product = products[position]
+        holder.bind(product)
     }
 
     override fun getItemCount(): Int {
