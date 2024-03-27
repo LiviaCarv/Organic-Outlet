744ccf9751ea4c2036109938f8b1ceff14ed9c58
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-03-27 16:30:09 -0400

    changed product_item layout

--- app/src/main/java/com/project/organicoutlet/dao/ProductsDao.kt
+++ app/src/main/java/com/project/organicoutlet/dao/ProductsDao.kt
@@ -2,19 +2,19 @@ package com.project.organicoutlet.dao
 
 
 import com.project.organicoutlet.model.Product
-
+import java.math.BigDecimal
 
 class ProductsDao {
     companion object {
-        private val products = mutableListOf<Product>()
+        private val products =
+            mutableListOf<Product>(Product("Cesta", "frutas", BigDecimal("19.99")))
     }
-
 
     fun insert(product: Product) {
         products.add(product)
     }
 
-    fun getAllProducts() : List<Product> {
+    fun getAllProducts(): List<Product> {
         return products.toList()
     }
 }
