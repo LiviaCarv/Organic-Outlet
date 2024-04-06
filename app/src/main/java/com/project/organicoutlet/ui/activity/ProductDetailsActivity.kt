e2aa7d0bcd4c2c12d87b9fa2b042617c49d45b37
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-04-06 08:36:01 -0400

    initialized onOptionsItemSelected implementation

--- app/src/main/java/com/project/organicoutlet/ui/activity/ProductDetailsActivity.kt
+++ app/src/main/java/com/project/organicoutlet/ui/activity/ProductDetailsActivity.kt
@@ -3,14 +3,19 @@ package com.project.organicoutlet.ui.activity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
 import com.project.organicoutlet.R
 import com.project.organicoutlet.databinding.ActivityProductDetailsBinding
 import com.project.organicoutlet.extensions.loadImage
 import com.project.organicoutlet.database.Product
+import com.project.organicoutlet.database.ProductDatabase
 
 
 class ProductDetailsActivity : AppCompatActivity() {
+
+    private lateinit var currentProduct: Product
 
     private val binding by lazy {
         ActivityProductDetailsBinding.inflate(layoutInflater)
@@ -22,6 +27,7 @@ class ProductDetailsActivity : AppCompatActivity() {
 
         val product = intent.getSerializableExtra("product") as? Product
         product?.let {
+            currentProduct = product
             binding.product = product
             binding.imgProductTop.loadImage(product.image)
         }
@@ -33,5 +39,23 @@ class ProductDetailsActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val database = ProductDatabase.getInstance(this)
+        val productDao = database.productDao()
+        return when (item.itemId) {
+            R.id.option_edit -> {
+                productDao.update(currentProduct)
+                true
+            }
+
+            R.id.option_delete -> {
+                productDao.delete(currentProduct)
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 }
